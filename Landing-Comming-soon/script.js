a30ab07889aceed83f88577bb88dfbfc35861b73
Vivek Ranchhod Patel <121936975+VivekPatel2117@users.noreply.github.com>
2024-01-14 16:54:42 +0530

    Add files via upload

---
+++ Landing-Comming-soon/script.js
@@ -0,0 +1,60 @@
+
+const dateBtn = document.getElementById('btn');
+const datepicker = document.getElementById('date');
+const hoursCount = document.getElementById('hourscount');
+const minutesCount = document.getElementById('minutescount');
+const secondsCount = document.getElementById('secondscount');
+
+// let userDateinput;
+
+function showdate() {
+    dateBtn.style.display = "none";
+    datepicker.style.display = "block";
+}
+
+const updateTime = () => {
+    datepicker.style.display = "none";
+    userDateinput = new Date(datepicker.value);
+    dateBtn.style.display = "block";
+
+    const userDate = userDateinput.toLocaleString();
+    const currentTime = new Date();
+    const systemDate = currentTime.toLocaleString();
+    console.log("userDate=" + userDate);
+    console.log("SystemDate=" + systemDate);
+
+    // Get the user-input date
+    if (isNaN(userDateinput.getTime())) {
+        datepicker.value="";
+        alert("Select a Date");
+        console.log("Invalid DATE");
+        return;
+    }
+
+    // Compare the two dates
+    if (userDateinput < currentTime) {
+        datepicker.value="";
+        alert("Please enter a future date");
+        console.log("past date");
+        return;
+    } else {
+        // Calculate the time difference
+        const timeDifference = userDateinput.getTime() - currentTime.getTime();
+
+        // Convert the time difference to hours, minutes, and seconds
+        const hours = Math.floor(timeDifference / (1000 * 60 * 60));
+        hoursCount.innerText = hours;
+        const minutes = Math.floor((timeDifference % (1000 * 60 * 60)) / (1000 * 60));
+        minutesCount.innerHTML = minutes;
+        const seconds = Math.floor((timeDifference % (1000 * 60)) / 1000);
+        secondsCount.innerHTML = seconds;
+
+        console.log(hours + " " + minutes + " " + seconds);
+    }
+
+    // Call the function again after 1000 milliseconds (1 second)
+    setTimeout(updateTime, 1000);
+};
+
+// Initial call to start the process
+updateTime();
